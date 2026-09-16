d12d2b04e65a23273bd83ffe3af5f10fc68aeb43
gautam7373 <abhishek.gautam7373@gmail.com>
2026-09-17 04:14:14 +0530

    Add files via upload

---
+++ UserInput.py
@@ -0,0 +1,4 @@
+#program to take input from user
+print ("A Sample program to get name from user")
+name=input("Your Name: ")
+print ("Hello", name)
---
+++ basics1.py
@@ -0,0 +1,5 @@
+name = "abhsihek"
+age1 = 25
+age2 = 35
+print("Actual Age", age2)
+print("Prev Data", age1)
