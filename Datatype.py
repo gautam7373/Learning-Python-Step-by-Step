6723f42904f9a3ff96ad8bbfc2a315e0b489601a
gautam7373 <abhishek.gautam7373@gmail.com>
2026-09-18 05:56:31 +0530

    Its Day 2.. of Python learning

---
+++ Datatype.py
@@ -0,0 +1,10 @@
+#datatype
+#program to take age as input¿print value and data type
+
+age = input("Enter your Age = ")
+cost = input("what is the cost of your phone = ")
+print("Your Age is = ", age)
+print("price of your phone is =", cost)
+
+print("Datatype is ", type(age))
+print("Dataype of price is = ", type(cost))
